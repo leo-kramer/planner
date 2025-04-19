3f3ba878c8d8e7c66e13a2320cb3ec14d4f60813
leo-kramer <leokramer17@gmail.com>
2025-04-19 20:31:09 +0200

    Integrate supabase DB

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import { useState } from 'react'
+import React from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.SUPA_URL as string;
+const apiKey = import.meta.env.SUPA_URL as API_KEY;
+
+const supabase = createClient(supabaseUrl, apiKey);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = React.useState([]);
+
+  React.useEffect(() => {
+    getTasks();
+  }, []);
+
+  async function getTasks() {
+    const { data, error } = await supabase.from('tasks').select(`
+        id,
+        name, 
+        property_values ( value, property:properties (*) )
+      `);
+
+    if (error) throw error;
+    console.log(data);
+    setTasks(data);
+  }
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {tasks.map(task => (
+        <li key={task.id}>
+          <h3>{task.name}</h3>
+          <p>{task.property_values[0].property.name}</p>
+          <p>{task.property_values[0].value}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default App
+export default App;
