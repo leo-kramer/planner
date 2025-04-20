66d1705d60ccdbc61e290e299f97961d7c81c81a
leo-kramer <leokramer17@gmail.com>
2025-04-20 11:04:20 +0200

    Set up PWA manifest

--- src/services/supabase/data/connect.tsx
+++ src/services/supabase/data/connect.tsx
@@ -1,13 +1,10 @@
-// src/services/supabase/connect.tsx
 import React, { ReactNode } from 'react';
-import { SupabaseContext, supabase } from './client'; // Import the supabase client and context from client.tsx
+import { SupabaseContext, supabase } from './client';
 
-// Type for DbProvider component props
 interface DbProviderProps {
   children: ReactNode;
 }
 
-// DbProvider component that wraps children with context
 export const DbProvider: React.FC<DbProviderProps> = ({ children }) => {
   return <SupabaseContext.Provider value={{ supabase }}>{children}</SupabaseContext.Provider>;
 };
