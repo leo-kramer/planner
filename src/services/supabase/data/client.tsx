e5eead405ac73a56fd0f3d130b4ff12ef921f098
leo-kramer <leokramer17@gmail.com>
2025-04-20 08:36:13 +0200

    Add db connection

---
+++ src/services/supabase/data/client.tsx
@@ -0,0 +1,22 @@
+// src/services/supabase/client.tsx
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { createContext, useContext } from 'react';
+
+// Supabase client setup
+const supabaseUrl = import.meta.env.VITE_SUPA_URL as string;
+const apiKey = import.meta.env.VITE_API_KEY as string;
+
+// Export the Supabase client for use elsewhere
+export const supabase: SupabaseClient = createClient(supabaseUrl, apiKey);
+
+// Supabase context setup
+export const SupabaseContext = createContext<{ supabase: SupabaseClient } | undefined>(undefined);
+
+// Hook to access the Supabase context
+export const useDb = () => {
+  const context = useContext(SupabaseContext);
+  if (!context) {
+    throw new Error('useDb must be used within a DbProvider');
+  }
+  return context.supabase; // Now returns the supabase client directly
+};
